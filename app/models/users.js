58e27393bc720a6776ca268b4c170dc62d318441
1127071993@qq.com <aini2009626>
2019-09-26 21:42:33 +0800

    10-2： 个人资料的 schema 设计

--- app/models/users.js
+++ app/models/users.js
@@ -4,7 +4,25 @@ const userSchema = new Schema({
   //select 可以隐藏字段
   __v:{type:Number,select:false},
   name:{type:String,required:true},
-  password:{type:String,required:true,select:false}
+  password:{type:String,required:true,select:false},
   // age:{type:Number,required:false,default:0}
+  avatar_url:{type:String},
+  gender:{type:String,enum:['male','female'],default:'male',required:true},
+  headline:{type:String},
+  locations:{type:[{type:String}]},
+  business:{type:String},
+  employments:{type:[{
+    company:{type:String},
+    job:{type:String}
+  }]},
+  educations:{
+    type:[{
+      school:String,
+      major:String,
+      diploma:{type:String,enum:[1,2,3,4,5,]},
+      entrance_year:Number,
+      graduation_year:Number
+    }]
+  }
 })
 module.exports = model('User',userSchema)
